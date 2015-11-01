13e944c3747b6dc6df5643aad3f5d5e79444b0b5
Will Estes <squiffypwn@gmail.com>
2015-10-31 21:30:14 -0400

    Add more zsh and vim config

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # dotfiles
 My new and improved dotfiles
+
+## Color schemes
+This repo uses Base-16 Paraiso Dark color scheme. Go to [this repo](https://github.com/chriskempson/base16) to get the color schemes for your terminal.
+
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,19 +2,35 @@
 
 echo "Removing old dotfiles..."
 rm -rf ~/.vimrc
+rm -rf ~/.vim
 rm -rf ~/.zshrc
 rm -rf ~/.oh-my-zsh
+rm -rf ~/.config
 
 # Get directory of dotfiles
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Install vimrc
-echo "Installing vimrc"
+echo "Installing vim configs..."
 ln -s $DIR/vim/vimrc ~/.vimrc
+
+# Installing vim configs
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Install base16 vim plugin
+cd ~/.vim/bundle
+git clone https://github.com/chriskempson/base16-vim.git
+cd $DIR
 
 # Install oh my zsh
 echo "Installing Oh My ZSH"
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 chsh -s `which zsh`
+
+# Install zshrc
+ln -s $DIR/zsh/zshrc ~/.zshrc
+
+# Install base16-shell
+git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
